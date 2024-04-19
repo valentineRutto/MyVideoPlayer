3ad055edf36ed9f42771994c16291ec6ff1fa6b4
valentinerutto <rutto28@gmail.com>
2024-04-20 00:26:33 +0300

    -Refactor listscreen ui

--- app/src/main/java/com/valentinerutto/myvideoplayer/ui/VideoListScreen.kt
+++ app/src/main/java/com/valentinerutto/myvideoplayer/ui/VideoListScreen.kt
@@ -12,9 +12,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedAssistChip
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
